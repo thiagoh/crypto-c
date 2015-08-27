6228bceeb753469c3d19918e2ed2e0e9ba856a2a
Thiago Andrade <thiagoh@gmail.com>
2015-08-27 02:17:05 -0300

    base64 functions working and passing all tests and optimized on memory aspects

--- test/tests.c
+++ test/tests.c
@@ -95,10 +95,11 @@ static void _test_success_iv(cryptoc_cipher_type type, const unsigned char *key,
 
 static void _test_success_base64(const unsigned char *plain, int length) {
 
-	unsigned char* dataEncoded = (unsigned char*) malloc(sizeof(unsigned char) * (length + 32));
+	// http://stackoverflow.com/questions/13378815/base64-length-calculation
+	unsigned char* dataEncoded = (unsigned char*) malloc(sizeof(unsigned char) * (4 * length/3));
 	int dataEncodedLen = cryptoc_base64_encode(plain, length, dataEncoded);
 
-	unsigned char* dataDecoded = (unsigned char*) malloc(sizeof(unsigned char) * (dataEncodedLen + 32));
+	unsigned char* dataDecoded = (unsigned char*) malloc(sizeof(unsigned char) * dataEncodedLen);
 	int dataDecodedLen = cryptoc_base64_decode(dataEncoded, dataEncodedLen, dataDecoded);
 
 	assert_int_equal(length, dataDecodedLen);
